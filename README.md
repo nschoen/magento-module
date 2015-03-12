5efca9edb2b0161ee6d179ab5ac15b018b312de0
Aarne Welschlau <aarne.welschlau@ratepay.com>
2015-03-12 16:31:07 +0100

    initial commit of version 3.0.0

---
+++ README.md
@@ -0,0 +1,13 @@
+RatePAY GmbH - Magento Payment Module
+============================================
+
+|Module | RatePAY Module for Magento
+|------|----------
+|Author | `Aarne Welschlau`
+|Prefix | `pi`
+|Shop Version | `CE` `<= 1.9.x`
+|Version | `3.0.0`
+|Link | `http://www.ratepay.com`
+|Mail | `integration@ratepay.com`
+|Description | ``
+|Installation | `see separate installation manual`
---
+++ app/code/community/RatePAY/Ratepaypayment/Block/Footer/Deviceident.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category RatePAY
+ * @package RatePAY_Ratepaypayment
+ * @copyright Copyright (c) 2015 RatePAY GmbH (https://www.ratepay.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+class RatePAY_Ratepaypayment_Block_Footer_Deviceident extends Mage_Core_Block_Template
+{
+    /**
+     * Construct
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('ratepay/footer/ditest.phtml')->toHtml();
+    }
+
+    public function getHello() {
+        $a2 = 2;
+    }
+
+}
